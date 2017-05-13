70914e4bc41670f0313579610aa858934b32d148
Claycorp <Claycorp@doubledoordev.net>
2017-05-12 23:32:26 -0500

    Update to 1.10.2, Added damage message config. Dries you will need to fix any Jenkins stuff. <3

--- src/main/java/net/doubledoordev/minesafety/MineSafety.java
+++ src/main/java/net/doubledoordev/minesafety/MineSafety.java
@@ -1,46 +1,52 @@
-package net.doubledoordev.minesafety;
+package net.doubledoordev;
 
-import cpw.mods.fml.client.config.IConfigElement;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.common.registry.LanguageRegistry;
-import net.doubledoordev.d3core.util.ID3Mod;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
-import org.lwjgl.Sys;
+import net.minecraftforge.fml.client.config.IConfigElement;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.io.File;
 import java.util.List;
 import java.util.Random;
 
-/**
- * @author Dries007
- */
-@Mod(modid = MineSafety.MODID)
-public class MineSafety implements ID3Mod
+@Mod(modid = MineSafety.MOD_ID, name = MineSafety.MOD_NAME, version = MineSafety.VERSION)
+public class MineSafety
 {
-    public static final String          MODID        = "MineSafety";
-    private             Random          random       = new Random();
-    private             DamageSource    damageSource = new DamageSource("helmet").setDifficultyScaled();
+    public static final String MOD_NAME = "MineSafety";
+    public static final String VERSION  = "1.1.0";
+    public static final String       MOD_ID        = "minesafety";
+    private                 Random       random       = new Random();
+    private                 DamageSource damageSource = new DamageSource("helmet").setDifficultyScaled();
     private             Configuration   configuration;
+    private File folder;
     private int yLevel;
     private float chance;
     private int timeout;
+    private String message;
 
-    @Mod.EventHandler
-    public void init(FMLPreInitializationEvent event)
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
     {
-        FMLCommonHandler.instance().bus().register(this);
+        //FMLCommonHandler.instance().bus().register(this);
+        MinecraftForge.EVENT_BUS.register(this);
 
-        configuration = new Configuration(event.getSuggestedConfigurationFile());
-        syncConfig();
+        folder = new File(event.getModConfigurationDirectory(), MOD_ID);
+        folder.mkdir();
+        configuration = new Configuration(new File(folder, event.getSuggestedConfigurationFile().getName()));
+        updateConfig();
     }
 
     @SubscribeEvent
@@ -48,41 +54,48 @@ public class MineSafety implements ID3Mod
     {
         if (event.phase == TickEvent.Phase.END) return;
         EntityPlayer player = event.player;
+
         NBTTagCompound data = player.getEntityData();
-        if (data.hasKey(MODID))
+        if (data.hasKey(MOD_ID))
         {
-            if (data.getInteger(MODID) == 0) data.removeTag(MODID);
-            else data.setInteger(MODID, data.getInteger(MODID) - 1);
+            if (data.getInteger(MOD_ID) == 0) data.removeTag(MOD_ID);
+            else data.setInteger(MOD_ID, data.getInteger(MOD_ID) - 1);
         }
         else
         {
             if (player.posY >= yLevel) return;
-            if (player.getCurrentArmor(3) != null && player.getCurrentArmor(3).getItem() instanceof ItemArmor && ((ItemArmor) player.getCurrentArmor(3).getItem()).armorType == 0) return;
-            if (player.worldObj.canBlockSeeTheSky((int) player.posX, (int) player.posY, (int) player.posZ)) return;
+            if (player.getItemStackFromSlot(EntityEquipmentSlot.HEAD) !=null && player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof  ItemArmor) return;
+            if (player.getEntityWorld().canBlockSeeSky(new BlockPos(player.posX, player.posY, player.posZ))) return;
             if (random.nextFloat() > chance) return;
             if (player.attackEntityFrom(damageSource, 1.0f + 0.2f * random.nextFloat()))
             {
-                player.addChatComponentMessage(new ChatComponentText(LanguageRegistry.instance().getStringLocalization("d3.mineSafety.message")));
-                data.setInteger(MODID, 20 * timeout);
+                player.sendStatusMessage(new TextComponentString(message));
+                data.setInteger(MOD_ID, 20 * timeout);
             }
         }
     }
 
-    @Override
-    public void syncConfig()
+    //@Override
+    public void addConfigElements(List<IConfigElement> configElements)
     {
-        configuration.setCategoryLanguageKey(MODID, "d3.mineSafety.config.mineSafety");
-        //public int getInt(String name, String category, int defaultValue, int minValue, int maxValue, String comment, String langKey)
-        yLevel = configuration.getInt("yLevel", MODID, 50, 0, 256, "The Y level at which you should wear a helmet.", "d3.mineSafety.config.yLevel");
-        chance = configuration.getFloat("chance", MODID, 0.03f, 0.0f, 1.0f, "The chance you get damaged this tick, in percent.", "d3.mineSafety.config.chance");
-        timeout = configuration.getInt("timeout", MODID, 1, 0, Integer.MAX_VALUE, "The minimum time between 2 hits from this mod", "d3.mineSafety.config.timeout");
+        configElements.add(new ConfigElement(configuration.getCategory(MOD_ID.toLowerCase())));
+    }
+
+    @SubscribeEvent
+    public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event){
+        if (event.getModID().equals(MOD_ID)) updateConfig();
+    }
+
+    private void updateConfig()
+    {
+        configuration.setCategoryLanguageKey(MOD_ID, "d3.mineSafety.config.mineSafety").setCategoryComment(MOD_ID, "MineSafety Configs");;
+        yLevel = configuration.getInt("yLevel", MOD_ID, 50, 0, 256, "The Y level at which you should wear a helmet.", "d3.mineSafety.config.yLevel");
+        chance = configuration.getFloat("chance", MOD_ID, 0.03f, 0.0f, 1.0f, "The chance you get damaged this tick, in percent.", "d3.mineSafety.config.chance");
+        timeout = configuration.getInt("timeout", MOD_ID, 1, 0, Integer.MAX_VALUE, "The minimum time between 2 hits from this mod", "d3.mineSafety.config.timeout");
+        message = configuration.getString("message", MOD_ID, "Ouch! Falling rocks... I should wear a helmet.","The message displayed ingame when user takes damage from no helmet", "d3.mineSafety.config.message");
 
         if (configuration.hasChanged()) configuration.save();
     }
 
-    @Override
-    public void addConfigElements(List<IConfigElement> configElements)
-    {
-        configElements.add(new ConfigElement(configuration.getCategory(MODID.toLowerCase())));
-    }
 }
+
