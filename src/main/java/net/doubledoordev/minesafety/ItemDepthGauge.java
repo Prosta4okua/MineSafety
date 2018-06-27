2a278470d7a1a4ceaa39cf5acaf250b8ce3725d4
Claycorp <Claycorp@doubledoordev.net>
2018-06-26 23:14:37 -0500

    Added Depth Gauge item, Displays Y level in top left corner of HUD while located in the inventory or hotbar. Off-Hand does not count.

---
+++ src/main/java/net/doubledoordev/minesafety/ItemDepthGauge.java
@@ -0,0 +1,14 @@
+package net.doubledoordev.minesafety;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+
+public class ItemDepthGauge extends Item
+{
+    public ItemDepthGauge()
+    {
+        this.setMaxStackSize(1);
+        this.setCreativeTab(CreativeTabs.MISC);
+    }
+}
--- src/main/java/net/doubledoordev/minesafety/MineSafety.java
+++ src/main/java/net/doubledoordev/minesafety/MineSafety.java
@@ -1,15 +1,23 @@
 package net.doubledoordev.minesafety;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -17,21 +25,50 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 @Mod(modid = MineSafety.MOD_ID, name = MineSafety.MOD_NAME, version = MineSafety.VERSION)
 public class MineSafety
 {
     public static final String MOD_NAME = "minesafety";
-    public static final String VERSION  = "1.2.0";
+    public static final String VERSION  = "1.3.0";
     public static final String MOD_ID = "minesafety";
     private Random random = new Random();
     private DamageSource damageSource = new DamageSource("helmet").setDifficultyScaled();
+    private Minecraft mc = Minecraft.getMinecraft();
+    private ItemDepthGauge depthGauge = new ItemDepthGauge();
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void registerItems(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(depthGauge.setRegistryName("depthGauge").setUnlocalizedName("depth_gauge"));
+    }
+
+    @SubscribeEvent
+    public void registerResources(ModelRegistryEvent event)
+    {
+        ModelLoader.setCustomModelResourceLocation(depthGauge,0,new ModelResourceLocation(depthGauge.getRegistryName(),"inventory"));
+    }
+
+    @SubscribeEvent
+    public void drawTextEvent(RenderGameOverlayEvent.Text event)
+    {
+        ArrayList<String> list = event.getLeft();
+        int yPos = mc.player.getPosition().getY();
+        for(int i=0; i < 35; i++)
+        {
+            if (mc.player.inventory.getStackInSlot(i).isItemEqual(new ItemStack(depthGauge)))
+            {
+                list.add("Y=" + yPos);
+            }
+        }
     }
 
     @SubscribeEvent
